9d29deed3c2fe511a883e2a7ad189b0e16c05c67
Charles Garcia <154024657+ch-garcia@users.noreply.github.com>
2025-04-24 13:44:37 +0200

    Update the solution

--- 30-days-pandas/0595-big-countries.py
+++ 30-days-pandas/0595-big-countries.py
@@ -55,5 +55,4 @@ Output:
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    df = world[(world["area"] >= 3000000) | (world["population"] >= 25000000)]
-    return df[["name","population","area"]]
+    return world[(world['area'] >= 3000000) | (world['population'] >= 25000000)][['name', 'population', 'area']]
