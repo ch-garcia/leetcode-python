7d825595887164735793c8f083b657e47c511baf
Charles Garcia <154024657+ch-garcia@users.noreply.github.com>
2025-04-26 10:27:20 +0200

    Update file with shorter solution

--- introduction_pandas/2884_modify_columns.py
+++ introduction_pandas/2884_modify_columns.py
@@ -45,5 +45,5 @@ Every salary has been doubled."
 import pandas as pd
 
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['salary'] = employees['salary'] * 2
+    employees['salary'] *= 2
     return employees
